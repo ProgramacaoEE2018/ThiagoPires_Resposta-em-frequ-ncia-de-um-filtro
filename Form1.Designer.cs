64065c03b6343ce4ab3ac89e4e05f6028c0d89d4
Thiagoeiji <37377082+Thiagoeiji@users.noreply.github.com>
2018-06-24 16:46:21 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace AppFiltro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.bconect = new System.Windows.Forms.Button();
+            this.bcaptr = new System.Windows.Forms.Button();
+            this.bclean = new System.Windows.Forms.Button();
+            this.grafmod = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.graffase = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.grafmod)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.graffase)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bconect
+            // 
+            this.bconect.Location = new System.Drawing.Point(52, 87);
+            this.bconect.Name = "bconect";
+            this.bconect.Size = new System.Drawing.Size(75, 23);
+            this.bconect.TabIndex = 0;
+            this.bconect.Text = "Conectar";
+            this.bconect.UseVisualStyleBackColor = true;
+            this.bconect.Click += new System.EventHandler(this.bconect_Click);
+            // 
+            // bcaptr
+            // 
+            this.bcaptr.Location = new System.Drawing.Point(52, 175);
+            this.bcaptr.Name = "bcaptr";
+            this.bcaptr.Size = new System.Drawing.Size(75, 23);
+            this.bcaptr.TabIndex = 1;
+            this.bcaptr.Text = "Capturar";
+            this.bcaptr.UseVisualStyleBackColor = true;
+            this.bcaptr.Click += new System.EventHandler(this.bcaptr_Click);
+            // 
+            // bclean
+            // 
+            this.bclean.Location = new System.Drawing.Point(52, 262);
+            this.bclean.Name = "bclean";
+            this.bclean.Size = new System.Drawing.Size(75, 23);
+            this.bclean.TabIndex = 2;
+            this.bclean.Text = "Limpar";
+            this.bclean.UseVisualStyleBackColor = true;
+            this.bclean.Click += new System.EventHandler(this.bclean_Click);
+            // 
+            // grafmod
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.grafmod.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.grafmod.Legends.Add(legend1);
+            this.grafmod.Location = new System.Drawing.Point(349, 87);
+            this.grafmod.Name = "grafmod";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.grafmod.Series.Add(series1);
+            this.grafmod.Size = new System.Drawing.Size(439, 300);
+            this.grafmod.TabIndex = 3;
+            this.grafmod.Text = "chart1";
+            // 
+            // graffase
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.graffase.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.graffase.Legends.Add(legend2);
+            this.graffase.Location = new System.Drawing.Point(349, 393);
+            this.graffase.Name = "graffase";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.graffase.Series.Add(series2);
+            this.graffase.Size = new System.Drawing.Size(439, 300);
+            this.graffase.TabIndex = 4;
+            this.graffase.Text = "chart2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Copperplate Gothic Bold", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(417, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(308, 33);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Diagrama de Bode";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(188, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Verifica a conexão com o osciloscópio";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(305, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Inicia a medição (somente se o osciloscópio estiver conectado)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 237);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(150, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Limpa os resultados do gráfico";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 697);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.graffase);
+            this.Controls.Add(this.grafmod);
+            this.Controls.Add(this.bclean);
+            this.Controls.Add(this.bcaptr);
+            this.Controls.Add(this.bconect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.grafmod)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.graffase)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bconect;
+        private System.Windows.Forms.Button bcaptr;
+        private System.Windows.Forms.Button bclean;
+        private System.Windows.Forms.DataVisualization.Charting.Chart grafmod;
+        private System.Windows.Forms.DataVisualization.Charting.Chart graffase;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,388 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Ivi.Visa.Interop;
+using System.Runtime.InteropServices;
+using System.Globalization;
+
+
+namespace AppFiltro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+
+            bcaptr.Enabled = false;
+            bclean.Enabled = false;
+            
+        }
+
+        private static VisaComInstrument myScope;
+
+
+        /*
+       * Initialize the oscilloscope to a known state.
+       * --------------------------------------------------------------
+       */
+        private static void Initialize()
+        {
+            string strResults;
+            // Get and display the device's *IDN? string.
+            strResults = myScope.DoQueryString("*IDN?");
+            MessageBox.Show("*IDN? result is: " + strResults, "Inicialização do Osciloscópio");
+            // Clear status and load the default setup.
+            myScope.DoCommand("*CLS");
+            myScope.DoCommand("*RST");
+
+        }
+
+        //Setups iniciais
+        private static void Setup()
+        {
+            // Use auto-scale to automatically configure oscilloscope.
+            myScope.DoCommand(":AUToscale");
+
+            // Set trigger mode (EDGE, PULSe, PATTern, etc., and input source.
+            myScope.DoCommand(":TRIGger:MODE EDGE");
+
+            // Set EDGE trigger parameters.
+            myScope.DoCommand(":TRIGger:EDGE:SOURCe CHANnel1");
+            myScope.DoCommand(":TRIGger:EDGE:LEVel 1.0");
+            myScope.DoCommand(":TRIGger:EDGE:SLOPe POSitive");
+
+            // Save oscilloscope configuration.
+            byte[] ResultsArray; // Results array.
+            int nLength; // Number of bytes returned from instrument.
+            string strPath;
+
+            // Query and read setup string.
+            ResultsArray = myScope.DoQueryIEEEBlock(":SYSTem:SETup?");
+            nLength = ResultsArray.Length;
+
+            // Write setup string to file.
+            strPath = "c:\\scope\\config\\setup.stp";
+            FileStream fStream = File.Open(strPath, FileMode.Create);
+            fStream.Write(ResultsArray, 0, nLength);
+            fStream.Close();
+
+            // Change settings with individual commands:
+            // Set vertical scale and offset.
+
+            // Set horizontal scale and position.
+
+            // Set the acquisition type (NORMal, PEAK, AVERage, or HRESolution
+            myScope.DoCommand(":ACQuire:TYPE NORMal");
+
+            // Or, configure by loading a previously saved setup.
+            byte[] DataArray;
+            int nBytesWritten;
+
+            // Read setup string from file.
+            strPath = "c:\\scope\\config\\setup.stp";
+            DataArray = File.ReadAllBytes(strPath);
+            nBytesWritten = DataArray.Length;
+
+            // Restore setup string.
+            myScope.DoCommandIEEEBlock(":SYSTem:SETup", DataArray);
+            Console.WriteLine("Setup bytes restored: {0}", nBytesWritten);
+
+
+
+        }
+
+        static byte[] Freq = new byte[10000000]; static byte[] Ganho = new byte[10000000]; static byte[] Fase = new byte[10000000];
+
+        //Configura o setup do gerador de funções
+        private static void GerarOnda()
+        {
+            myScope.DoCommand(":WGEN:FUNCtion SINusoid");
+            myScope.DoCommand(":WGEN:VOLTage:OFFSet 0.0");
+            myScope.DoCommand(":WGEN:VOLTage 9.0");
+            myScope.DoCommand(":WGEN:OUTPut 1");
+        }
+
+        // Configura a frequência do gerador 
+        private static void SetFreq(int f)
+        {
+
+            myScope.DoCommand(":WGEN:FREQuency?" + f.ToString());
+
+        }
+
+        //Variável que indica se o osciloscópio está conectado
+
+        //LIMITE MAX E MIN DE FREQ
+        static int Fmax = 10000;
+        static int Fmin = 1;
+
+        //Vetores para gráfico
+        private static double[] freq = new double[Fmax + 10];
+        private static double[] Amp1 = new double[Fmax + 10];
+        private static double[] Amp2 = new double[Fmax + 10];
+        private static double[] phase = new double[Fmax + 10];
+        private static double[] modulodb = new double[Fmax + 10];
+
+        // Mede a amplitude das ondas de entrada e saída e a fase
+        private static void CapturaDados()
+        {
+
+            GerarOnda();
+
+            //Modo do indicador para medir voltagem
+            myScope.DoCommand(":MARKer:MODE WAVeform");
+
+            //Indica o Source de cada marker(Ch1 ou Ch2)
+            myScope.DoCommand(":MARKer:X1Y1source CHANnel1");
+            myScope.DoCommand(":MARKer:X2Y2source CHANnel2");
+
+            //Loop para adquirir dados de amplitude e frequência
+            int i;
+            for (i = Fmin; i <= Fmax; i++)
+            {
+                SetFreq(i);
+                freq[i] = i;
+                Amp1[i] = myScope.DoQueryNumber("MARKer:Y1Position?");
+                Amp2[i] = myScope.DoQueryNumber("MARKer:Y2Position?");
+
+            }
+
+            //Adquirir dados de fase
+            //Configurãção para medir fase
+            myScope.DoCommand(":MEASure: SOURce CHANnel1 ,CHANnel2");
+
+            int j;
+            for (j = Fmin; j <= Fmax; j++)
+            {
+                SetFreq(j);
+                myScope.DoCommand(":MEASure:PHASe CHANnel1 ,CHANnel2");
+                phase[j] = myScope.DoQueryNumber(":MEASure:PHASe? CHANnel1 ,CHANnel2");
+            }
+
+
+
+        }
+
+        //Botão que inicia a conexão
+        private void bconect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                myScope = new
+                    VisaComInstrument("USB0::0x0957::0x17A2::MY54450144::0::INSTR"
+                    );
+                myScope.SetTimeoutSeconds(10);
+                // Initialize - start from a known state.
+                Initialize();
+                bconect.Enabled = false;
+                bcaptr.Enabled = true;
+                bclean.Enabled = true;
+            }
+            catch (System.ApplicationException err)
+            {
+                MessageBox.Show("*** VISA COM Error : " + err.Message);
+            }
+            catch (System.SystemException err)
+            {
+                MessageBox.Show("*** System Error Message : " + err.Message);
+            }
+            catch (System.Exception err)
+            {
+                System.Diagnostics.Debug.Fail("Unexpected Error");
+                MessageBox.Show("*** Unexpected Error : " + err.Message);
+            }
+            finally
+            {
+               
+            }
+        }
+
+        //Botão que vai iniciar a captura
+        private void bcaptr_Click(object sender, EventArgs e)
+        {
+
+                CapturaDados();
+
+                //cálculo do módulo em dB e plot nos gráficos
+                int k = Fmin;
+                for (k = Fmin; k <= Fmax; k++)
+                {
+
+                    double razao = Math.Pow(Amp2[k] / Amp1[k], 2);
+                    double modulo = Math.Pow(razao, 0.5);
+                    modulodb[k] = 20 * Math.Log10(modulo);
+                    grafmod.Series[0].Points.AddY(modulodb[k]);
+                    graffase.Series[0].Points.AddY(phase[k]);
+                }
+         
+        }
+
+
+
+        //Botão que limpa o diagrama
+        private void bclean_Click(object sender, EventArgs e)
+        {     
+                grafmod.Series.Clear();
+                graffase.Series.Clear();
+        }
+
+
+
+    }
+
+
+    class VisaComInstrument
+    {
+        private ResourceManager m_ResourceManager;
+        private FormattedIO488 m_IoObject;
+        private string m_strVisaAddress;
+        // Constructor
+        public VisaComInstrument(string strVisaAddress)
+        {
+            // Save VISA address in member variable.
+            m_strVisaAddress = strVisaAddress;
+            // Open the default VISA COM IO object.
+            OpenIo();
+            // Clear the interface.
+            m_IoObject.IO.Clear();
+        }
+        public void DoCommand(string strCommand)
+        {
+            // Send the command.
+            m_IoObject.WriteString(strCommand, true);
+            // Check for inst errors.
+            CheckInstrumentErrors(strCommand);
+        }
+        public void DoCommandIEEEBlock(string strCommand,
+        byte[] DataArray)
+        {
+            // Send the command to the device.
+            m_IoObject.WriteIEEEBlock(strCommand, DataArray, true);
+            // Check for inst errors.
+            CheckInstrumentErrors(strCommand);
+        }
+        public string DoQueryString(string strQuery)
+        { // Send the query.
+            m_IoObject.WriteString(strQuery, true);
+            // Get the result string.
+            string strResults;
+            strResults = m_IoObject.ReadString();
+            // Check for inst errors.
+            CheckInstrumentErrors(strQuery);
+            // Return results string.
+            return strResults;
+        }
+        public double DoQueryNumber(string strQuery)
+        {
+            // Send the query.
+            m_IoObject.WriteString(strQuery, true);
+            // Get the result number.
+            double fResult;
+            fResult = (double)m_IoObject.ReadNumber(
+            IEEEASCIIType.ASCIIType_R8, true);
+            // Check for inst errors.
+            CheckInstrumentErrors(strQuery);
+            // Return result number.
+            return fResult;
+        }
+        public double[] DoQueryNumbers(string strQuery)
+        {
+            // Send the query.
+            m_IoObject.WriteString(strQuery, true);
+            // Get the result numbers.
+            double[] fResultsArray;
+            fResultsArray = (double[])m_IoObject.ReadList(
+            IEEEASCIIType.ASCIIType_R8, ",;");
+            // Check for inst errors.
+            CheckInstrumentErrors(strQuery);
+            // Return result numbers.
+            return fResultsArray;
+        }
+        public byte[] DoQueryIEEEBlock(string strQuery)
+        {
+            // Send the query.
+            m_IoObject.WriteString(strQuery, true);
+            // Get the results array.
+            System.Threading.Thread.Sleep(2000); // Delay before reading.
+            byte[] ResultsArray;
+            ResultsArray = (byte[])m_IoObject.ReadIEEEBlock(
+            IEEEBinaryType.BinaryType_UI1, false, true);
+            // Check for inst errors.
+            CheckInstrumentErrors(strQuery);
+            // Return results array.
+            return ResultsArray;
+        }
+        private void CheckInstrumentErrors(string strCommand)
+        {
+            // Check for instrument errors.
+            string strInstrumentError;
+            bool bFirstError = true;
+            do // While not "0,No error".
+            {
+                m_IoObject.WriteString(":SYSTem:ERRor?", true);
+                strInstrumentError = m_IoObject.ReadString();
+                if (!strInstrumentError.ToString().StartsWith("+0,"))
+                {
+                    if (bFirstError)
+                    {
+                        Console.WriteLine("ERROR(s) for command '{0}': ",
+                        strCommand);
+                        bFirstError = false;
+                    }
+                    Console.Write(strInstrumentError);
+                }
+            } while (!strInstrumentError.ToString().StartsWith("+0,"));
+        }
+        private void OpenIo()
+        {
+            m_ResourceManager = new ResourceManager();
+            m_IoObject = new FormattedIO488();
+            // Open the default VISA COM IO object.
+            try
+            {
+                m_IoObject.IO =
+                (IMessage)m_ResourceManager.Open(m_strVisaAddress,
+                AccessMode.NO_LOCK, 0, "");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred: {0}", e.Message);
+            }
+        }
+        public void SetTimeoutSeconds(int nSeconds)
+        {
+            m_IoObject.IO.Timeout = nSeconds * 1000;
+        }
+        public void Close()
+        {
+            try
+            {
+                m_IoObject.IO.Close();
+            }
+            catch { }
+            try
+            {
+                Marshal.ReleaseComObject(m_IoObject);
+            }
+            catch { }
+            try
+            {
+                Marshal.ReleaseComObject(m_ResourceManager);
+            }
+            catch { }
+        }
+    }
+
+
+}
+
+
